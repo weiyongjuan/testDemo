0079016b004964628c374d7d660b3b3ac0332769
weiyongjuan <weiyj9600@163.com>
2019-05-16 17:09:33 +0800

    配置

--- src/main/java/com/example/testDamo/TestDemoApplication.java
+++ src/main/java/com/example/testDamo/TestDemoApplication.java
@@ -2,11 +2,18 @@ package com.example.testDamo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TestDemoApplication {
+public class TestDemoApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(TestDemoApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TestDemoApplication.class);
     }
 }
 
